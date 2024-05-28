eab98c90a4245dc2394a91cd6eb65a6c6ec313ce
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-28 16:46:57 +0530

    Basic form using react-hook-form is completed

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -4,13 +4,23 @@ type FormFields = {
     email: string,
 }
 const Users = () => {
-    const { register } = useForm<FormFields>();
-    return <input {...register('name')} placeholder='Enter Name' />
+    const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({mode:'all'});
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('email', {
+            required: { value: true, message: 'Email is required' },
+            maxLength: { value: 8, message: 'Too many characters' },
+        })} placeholder='Enter Email' />
+        {errors?.email && <div style={{color: 'red'}}>{errors.email.message}</div>}
+    </form>
 }
 
 export default Users;
 
-
-// Note: 
+// Note:
 // register() => // required, ref, onBlur, disabled, name, min, maxLength, max, onChange
 // so that is the reason register is passed
+// Clean code
+// easy to maintain
